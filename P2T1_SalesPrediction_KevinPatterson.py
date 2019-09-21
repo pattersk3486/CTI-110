7640071c488045bc24468281571e47e7727a2905
pattersk3486 <55610582+pattersk3486@users.noreply.github.com>
2019-09-20 23:09:20 -0400

    Add files via upload

---
+++ P2T1_SalesPrediction_KevinPatterson.py
@@ -0,0 +1,15 @@
+#Sales prediction
+#9/16/19
+#CTI-110 P2T1 - Sales Prediction
+#Kevin Patterson
+#
+#Get the projected sales.
+total_sales = float(input('Enter the projected sales: '))
+
+#Calculate the profit as 23 precent to total sales.
+profit = total_sales * 0.23
+
+#Display the profit.
+print('The profit is $', format(profit, ',.2f'))
+
+input()
